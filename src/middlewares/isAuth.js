89ca7f9f10e882537fe454b8a7d87ed04a398935
andresh01 <andres85684@outlook.com>
2023-06-07 23:03:21 -0500

    users authorization added

--- src/middlewares/isAuth.js
+++ src/middlewares/isAuth.js
@@ -1,47 +1,46 @@
 const jwt = require("jsonwebtoken");
 
-exports.isAuthAdmin = (req, res, next) => {
-
-    const tokenJwt = req.headers.token;
-    try {
-        let decodeToken = jwt.verify(tokenJwt, process.env.JWT_SECRET_KEY)
-        
-        if(decodeToken.role === 1){
-            next()
-        } else{
-            res.status(401).json({
-                message: 'Access forbidden'
-            })
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(401);
-        res.send(error.message || 'Access forbidden');
-        return;
-    }
-
+exports.isAuth = (role) => {
+    if(!role){
+        return (req, res, next) => {
+            const tokenJwt = req.headers.token;
+            try {
+                let decodeToken = jwt.verify(tokenJwt, process.env.JWT_SECRET_KEY)
     
-}
-
-exports.isAuthUser = (req, res, next) => {
-
-    const tokenJwt = req.headers.token;
-    try {
-        let decodeToken = jwt.verify(tokenJwt, process.env.JWT_SECRET_KEY)
-        
-        if(decodeToken.role === 2){
-            next()
-        } else{
-            res.status(401).json({
-                message: 'Access forbidden'
-            })
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(401);
-        res.send(error.message || 'Access forbidden');
-        return;
-    }
-
+                if (decodeToken.role) {
+                    next()
+                } else {
+                    res.status(401).json({
+                        message: 'Access forbidden'
+                    })
+                }
+            } catch (error) {
+                console.log(error);
+                res.status(401);
+                res.send(error.message || 'Access forbidden');
+                return;
+            }
     
+        }
+    }
+    return (req, res, next) => {
+        const tokenJwt = req.headers.token;
+        try {
+            let decodeToken = jwt.verify(tokenJwt, process.env.JWT_SECRET_KEY)
+
+            if (decodeToken.role === role) {
+                next()
+            } else {
+                res.status(401).json({
+                    message: 'Access forbidden'
+                })
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(401);
+            res.send(error.message || 'Access forbidden');
+            return;
+        }
+
+    }
 }
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const { createProductValidation, updateProductValidation, getProductValidation, deleteProductValidation } = require('../validation/productValidation')
 const validatorHandler = require("../middlewares/validatorHandler");
 const { getAllProducts, getProductById, addNewProduct, updateProduct, deleteProduct } = require("../controllers/productController");
-const { isAuthAdmin, isAuthUser } = require('../middlewares/isAuth');
+const { isAuth } = require('../middlewares/isAuth');
 
 const routes = new Router();
 
@@ -11,11 +11,11 @@ const BASE = "/api/v1/products";
 
 routes.get(`${BASE}/health`, (_, res) => res.send("check")); //sirve para evaluar el tiempo de respuesta y saber la eficiencia del servidor
 
-routes.get(`${BASE}`, isAuthUser, getAllProducts)
-routes.get(`${BASE}/:id`, isAuthUser, validatorHandler(getProductValidation, "params"), getProductById)
-routes.post(BASE, isAuthAdmin, validatorHandler(createProductValidation, "body"), addNewProduct) 
-routes.patch(`${BASE}/:id`, isAuthAdmin, validatorHandler(updateProductValidation, "params"), updateProduct)
-routes.delete(`${BASE}/:id`, isAuthAdmin, validatorHandler(deleteProductValidation, "params") , deleteProduct)
+routes.get(`${BASE}`,isAuth(), getAllProducts)
+routes.get(`${BASE}/:id`, isAuth(), validatorHandler(getProductValidation, "params"), getProductById)
+routes.post(BASE, isAuth(1), validatorHandler(createProductValidation, "body"), addNewProduct) 
+routes.patch(`${BASE}/:id`, isAuth(1), validatorHandler(updateProductValidation, "params"), updateProduct)
+routes.delete(`${BASE}/:id`, isAuth(1), validatorHandler(deleteProductValidation, "params") , deleteProduct)
 
 
 module.exports = routes;
